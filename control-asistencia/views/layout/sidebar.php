43de6b4e46c4b5d0d9a8d8b3b3b27b8a6aa00bb7
arield1310 <arieldaquino487@gmail.com>
2026-02-16 09:51:47 -0600

    Refactor sidebar links and logo path

--- control-asistencia/views/layout/sidebar.php
+++ control-asistencia/views/layout/sidebar.php
@@ -1,16 +1,17 @@
 <div class="sidebar">
     <div class="sidebar-header">
-        <img src="/control-asistencia/img/Logo INCO.png" alt="Logo Instituto" class="sidebar-logo">
+        <img src="/img/Logo INCO.png" alt="Logo Instituto" class="sidebar-logo">
         <h2 class="sidebar-title">
             Instituto Nacional de <br> El Congo
         </h2>
     </div>
 
-    <a href="/control-asistencia/dashboard.php"><i class="bi bi-house-door-fill"></i> Principal</a>
-    <a href="/control-asistencia/views/secciones/crear.php"><i class="bi bi-plus-circle-fill"></i> Crear Sección</a>
-    <a href="/control-asistencia/views/alumnos/registro_masivo.php"><i class="bi bi-file-earmark-text-fill"></i> Registrar Sección</a>
-    <a href="/control-asistencia/views/alumnos/crear.php"><i class="bi bi-person-fill-add"></i> Registrar Alumno</a>
-    <a href="/control-asistencia/views/alumnos/listado.php"><i class="bi bi-people-fill"></i> Listado</a>
-    <a href="/control-asistencia/views/reportes/reporte_mensual.php"><i class="bi bi-clipboard-data-fill"></i> Reporte Mensual</a>
-    <a href="/control-asistencia/logout.php"><i class="bi bi-box-arrow-left"></i> Cerrar Sesión</a>
+    <a href="/dashboard.php"><i class="bi bi-house-door-fill"></i> Principal</a>
+    <a href="/views/secciones/crear.php"><i class="bi bi-plus-circle-fill"></i> Crear Sección</a>
+    <a href="/views/alumnos/registro_masivo.php"><i class="bi bi-file-earmark-text-fill"></i> Registrar Sección</a>
+    <a href="/views/alumnos/crear.php"><i class="bi bi-person-fill-add"></i> Registrar Alumno</a>
+    <a href="/views/alumnos/listado.php"><i class="bi bi-people-fill"></i> Listado</a>
+    <a href="/views/reportes/reporte_mensual.php"><i class="bi bi-clipboard-data-fill"></i> Reporte Mensual</a>
+    <a href="/logout.php"><i class="bi bi-box-arrow-left"></i> Cerrar Sesión</a>
+
 </div>
