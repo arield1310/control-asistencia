9376099ba84ae01fe23b0d96a2183b3ea5a6ce97
arield1310 <arieldaquino487@gmail.com>
2026-02-16 10:33:57 -0600

    Add NIE field to student registration process

--- control-asistencia/controllers/guardar_alumno.php
+++ control-asistencia/controllers/guardar_alumno.php
@@ -7,25 +7,27 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     exit;
 }
 
+$nie = $_POST["nie"];
 $nombre = $_POST["nombre"];
 $apellido = $_POST["apellido"];
 $anio_id = $_POST["anio_id"];
 $modalidad_id = $_POST["modalidad_id"];
 $seccion_id = $_POST["seccion_id"];
 
-$stmt = $pdo->prepare("
-    INSERT INTO alumnos (nombre, apellido, anio_id, modalidad_id, seccion_id)
-    VALUES (:nombre, :apellido, :anio_id, :modalidad_id, :seccion_id)
-");
+$stmt = $pdo->prepare("INSERT INTO alumnos 
+(nie, nombre, apellido, anio_id, modalidad_id, seccion_id) 
+VALUES (:nombre, :apellido, :nie, :anio_id, :modalidad_id, :seccion_id)");
 
 $stmt->execute([
-    "nombre" => $nombre,
-    "apellido" => $apellido,
-    "anio_id" => $anio_id,
-    "modalidad_id" => $modalidad_id,
-    "seccion_id" => $seccion_id
+    'nie' => $_POST['nie'],
+    'nombre' => $_POST['nombre'],
+    'apellido' => $_POST['apellido'],
+    'anio_id' => $_POST['anio_id'],
+    'modalidad_id' => $_POST['modalidad_id'],
+    'seccion_id' => $_POST['seccion_id']
 ]);
 
 echo "Alumno registrado correctamente.";
 echo "<br><a href='../views/alumnos/crear.php'>Registrar otro</a>";
 echo "<br><a href='../dashboard.php'>Ir al Dashboard</a>";
+
