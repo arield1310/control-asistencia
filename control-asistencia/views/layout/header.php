6f69f8e1682ce0964414896a97ec3ec7956179e4
arield1310 <arieldaquino487@gmail.com>
2026-02-16 19:21:27 -0600

    Fix HTML structure in header.php

--- control-asistencia/views/layout/header.php
+++ control-asistencia/views/layout/header.php
@@ -11,9 +11,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="/public/css/admin.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <button class="menu-toggle" onclick="toggleMenu()"><i class="bi bi-list"></i></button>
 </head>
 <body>
+    <button class="menu-toggle" onclick="toggleMenu()"><i class="bi bi-list"></i></button>
 
 
 <div class="admin-container">
+
