23bc2ffa602e96ca8a78a713f37a03c19037f773
arield1310 <arieldaquino487@gmail.com>
2026-02-18 05:57:52 -0600

    Update index.php

--- control-asistencia/index.php
+++ control-asistencia/index.php
@@ -31,7 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Login - Instituto Nacional de El Congo</title>
     <link rel="stylesheet" href="/public/css/admin.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body class="login-body">
 
@@ -75,6 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 </body>
 </html>
+
 
 
 
